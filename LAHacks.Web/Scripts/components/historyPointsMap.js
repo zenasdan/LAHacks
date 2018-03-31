e37a29cad726f555abcd61a210d2241c598d81d4
Gowri Jagadeesh <gowrimj2006@gmail.com>
2018-03-31 13:04:06 -0700

    displaying map marker for different historic places around

--- LAHacks.Web/Scripts/components/historyPointsMap.js
+++ LAHacks.Web/Scripts/components/historyPointsMap.js
@@ -8,7 +8,7 @@
             vm.$onInit = _init;
             vm.mapOptions = {
                 center: null,
-                zoom: 18
+                zoom: 12
             };
 
             function _init() {
@@ -19,16 +19,40 @@
                 vm.latitude = 34.0705;
                 vm.longitude = -118.4468;
                 vm.mapOptions.center = new google.maps.LatLng(vm.latitude, vm.longitude);
+                var myCurrentPlaceIcon = {
+                    url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+                    scaledSize: new google.maps.Size(40, 40),
+                };
+                var venueIcon = {
+                    url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+                    scaledSize: new google.maps.Size(40, 40),
+                };
 
                 vm.map = new google.maps.Map(document.getElementById('gmap'), vm.mapOptions);
                 var marker = new google.maps.Marker({
                     map: vm.map,
                     position: new google.maps.LatLng(vm.latitude, vm.longitude),
-                    title: "historyPointsMap"
+                    icon: myCurrentPlaceIcon,
+                    title: "I'm Here"
                 });
-                google.maps.event.trigger(vm.map, 'resize');
 
                 foursquareService.getVenuesByHistoricCategory();
+                const venues = JSON.parse(localStorage.getItem("venues"));
+
+                for (const obj of venues) {
+                    console.log(obj);
+                    new google.maps.Marker({
+                        map: vm.map,
+                        position: new google.maps.LatLng(obj.venue.location.lat, obj.venue.location.lng),
+                        //icon: `${obj.venue.categories[0].icon.prefix}32${obj.venue.categories[0].icon.suffix}`,
+                        icon: venueIcon,
+                        title: obj.venue.name
+                    });
+                }
+                
+                google.maps.event.trigger(vm.map, 'resize');
+
+                
             }
         }
     });
