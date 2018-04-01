1eaf6e363e8434da40d93dfa66a88fac98693cbe
Zenas Dan <zenasdan@gmail.com>
2018-03-31 21:36:18 -0700

    working on search history

--- LAHacks.Web/Scripts/components/locationSearch.js
+++ LAHacks.Web/Scripts/components/locationSearch.js
@@ -13,7 +13,7 @@
             var syncObject = $firebaseObject(ref);
             // synchronize the object with a three-way data binding
             // click on `index.html` above to see it used in the DOM!
-            syncObject.$bindTo($scope, "data");
+            syncObject.$bindTo($scope, "locations");
 
             function _init() {
                 console.log("Firing");
@@ -21,7 +21,7 @@
             }
 
             function _search() {
-                ref.push($scope.data.text);
+                ref.push($scope.locations.text);
             }
         }
     })
