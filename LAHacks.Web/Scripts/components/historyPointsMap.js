89c187f02317152ebc3501f7a221bdea11a5871a
Zenas Dan <zenasdan@gmail.com>
2018-04-23 05:27:23 -0700

    made popular section dynamic. need to make it listen to changes in the map

--- LAHacks.Web/Scripts/components/historyPointsMap.js
+++ LAHacks.Web/Scripts/components/historyPointsMap.js
@@ -27,59 +27,17 @@
                 if ((lat.previousValue != lat.currentValue) || (long.previousValue != long.currentValue)) {
                     console.log("LAT OBJ: " + JSON.stringify(lat) + "; LONG OBJ: " + JSON.stringify(long));
                     _search(lat.currentValue, long.currentValue);
-                    //_googleMapResizeBugFix(lat.currentValue, long.currentValue);
                 }
             }
-
-            //var map, infoWindow;
-            //function initMap() {
-            //    map = new google.maps.Map(document.getElementById('map'), {
-            //        center: { lat: -34.397, lng: 150.644 },
-            //        zoom: 6
-            //    });
-            //    infoWindow = new google.maps.InfoWindow;
-
-            //    // Try HTML5 geolocation.
-            //    if (navigator.geolocation) {
-            //        navigator.geolocation.getCurrentPosition(function (position) {
-            //            var pos = {
-            //                lat: position.coords.latitude,
-            //                lng: position.coords.longitude
-            //            };
-
-            //            infoWindow.setPosition(pos);
-            //            infoWindow.setContent('Location found.');
-            //            infoWindow.open(map);
-            //            map.setCenter(pos);
-            //        }, function () {
-            //            handleLocationError(true, infoWindow, map.getCenter());
-            //        });
-            //    } else {
-            //        // Browser doesn't support Geolocation
-            //        handleLocationError(false, infoWindow, map.getCenter());
-            //    }
-            //}
-
-            //function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-            //    infoWindow.setPosition(pos);
-            //    infoWindow.setContent(browserHasGeolocation ?
-            //        'Error: The Geolocation service failed.' :
-            //        'Error: Your browser doesn\'t support geolocation.');
-            //    infoWindow.open(map);
-            //}
 
             function _init() {
                 console.log("LAT AND LONG IN INIT: " + JSON.stringify(vm.lat) + ", " + JSON.stringify(vm.long));
                 initMap();
-                //vm.lat = 34.0705;
-                //vm.long = -118.4468;
-                //_search(vm.lat, vm.long);
             }
 
             function initMap() {
                 infoWindow = new google.maps.InfoWindow;
 
-                // Try HTML5 geolocation.
                 if (navigator.geolocation) {
                     navigator.geolocation.getCurrentPosition(function (position) {
                         _search(position.coords.latitude, position.coords.longitude);
@@ -87,7 +45,6 @@
                         handleLocationError(true, infoWindow, map.getCenter());
                     });
                 } else {
-                    // Browser doesn't support Geolocation
                     handleLocationError(false, infoWindow, map.getCenter());
                 }
             }
@@ -126,33 +83,26 @@
                             const venues = JSON.parse(localStorage.getItem("venues"));
 
                             for (const obj of venues) {
+                                if (obj.venue.photos.groups[0]) {
+                                    let contentString = `<div id='content'>
+                                    <h4 class="text-center">${obj.venue.name}</h4>
+                                    <div><img src=${obj.venue.photos.groups[0].items[0].prefix}100x100${obj.venue.photos.groups[0].items[0].suffix} /><br/>`;
 
-                                let contentString = `<div id='content'>
-                                <h4 class="text-center">${obj.venue.name}</h4>
-                                <div><img src=${obj.venue.photos.groups[0].items[0].prefix}100x100${obj.venue.photos.groups[0].items[0].suffix} /><br/>`;
+                                    if (obj.tips) {
+                                        contentString += `<div>${obj.tips[0].text}</div>`;
+                                    }
 
-                                if (obj.tips) {
-                                    contentString += `<div>${obj.tips[0].text}</div>`;
+                                    contentString += `</div><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(obj.venue.name)}" target="_blank"> 
+                                    https://en.wikipedia.org/wiki/${obj.venue.name}</a></div > `;
+
+                                    let infowindow = new google.maps.InfoWindow({
+                                        content: contentString
+                                    });
                                 }
-
-                                contentString += `</div><a href="https://en.wikipedia.org/wiki/${encodeURIComponent(obj.venue.name)}" target="_blank"> 
-                                https://en.wikipedia.org/wiki/${obj.venue.name}</a></div > `;
-
-                                let infowindow = new google.maps.InfoWindow({
-                                    content: contentString
-                                });
 
                                 let marker = new google.maps.Marker({
                                     map: vm.map,
                                     position: new google.maps.LatLng(obj.venue.location.lat, obj.venue.location.lng),
-                                    //icon: `${obj.venue.categories[0].icon.prefix}32${obj.venue.categories[0].icon.suffix}|ddd`,
-                                    //icon: {
-                                    //    url: `${obj.venue.categories[0].icon.prefix}32${obj.venue.categories[0].icon.suffix}`,
-                                    //    strokeColor: 'blue',
-                                    //    scale: 3,
-                                    //    fillColor: '#0ff',
-                                    //    fillOpacity: 1,
-                                    //},
                                     icon: venueIcon,
                                     title: obj.venue.name
                                 });
@@ -166,15 +116,6 @@
                         });
                 }
             }
-
-            //function _googleMapResizeBugFix(lat, long) {
-                
-            //    google.maps.event.addListenerOnce(vm.map, 'idle', function () {
-            //        _search(lat, long);
-            //        google.maps.event.trigger(vm.map, 'resize');
-            //    });
-            //    console.log("RESIZING FINISHED");
-            //}
 
         }
     });
